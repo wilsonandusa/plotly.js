3d5bfcd76256b869f04c29161a9778bc4febdf99
Ricky Reusser <rsreusser@gmail.com>
2017-01-18 16:46:16 -0500

    Still update layout objects if there's no transitioned axis

--- src/plots/cartesian/transition_axes.js
+++ src/plots/cartesian/transition_axes.js
@@ -100,7 +100,23 @@ module.exports = function transitionAxes(gd, newLayout, transitionOpts, makeOnCo
     var updatedAxisIds = Object.keys(updates);
     var affectedSubplots = computeAffectedSubplots(fullLayout, updatedAxisIds, updates);
 
+    function updateLayoutObjs () {
+        function redrawObjs(objArray, method) {
+            var i;
+            for(i = 0; i < objArray.length; i++) {
+                var obji = objArray[i];
+
+                method(gd, i);
+            }
+        }
+
+        redrawObjs(fullLayout.annotations || [], Registry.getComponentMethod('annotations', 'drawOne'));
+        redrawObjs(fullLayout.shapes || [], Registry.getComponentMethod('shapes', 'drawOne'));
+        redrawObjs(fullLayout.images || [], Registry.getComponentMethod('images', 'draw'));
+    }
+
     if(!affectedSubplots.length) {
+        updateLayoutObjs();
         return false;
     }
 
@@ -195,7 +211,6 @@ module.exports = function transitionAxes(gd, newLayout, transitionOpts, makeOnCo
         }
 
         ticksAndAnnotations(subplot.xaxis, subplot.yaxis);
-
 
         var xa2 = subplot.xaxis;
         var ya2 = subplot.yaxis;
