95fa88d47bf448cfe9ebbd35098e1083c90a26d2
Étienne Tétreault-Pinard <etienne@plot.ly>
2017-06-15 10:47:38 -0400

    try

--- tasks/ci_test.sh
+++ tasks/ci_test.sh
@@ -5,17 +5,17 @@ EXIT_STATE=0
 case $CIRCLE_NODE_INDEX in
 
     0)
-        npm run test-image      || EXIT_STATE=$?
-        npm run test-image-gl2d || EXIT_STATE=$?
-        npm run test-bundle     || EXIT_STATE=$?
-        npm run test-syntax     || EXIT_STATE=$?
+#         npm run test-image      || EXIT_STATE=$?
+#         npm run test-image-gl2d || EXIT_STATE=$?
+#         npm run test-bundle     || EXIT_STATE=$?
+#         npm run test-syntax     || EXIT_STATE=$?
         npm run lint            || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
 
     1)
         npm run test-jasmine || EXIT_STATE=$?
-        npm run test-export  || EXIT_STATE=$?
+#         npm run test-export  || EXIT_STATE=$?
         exit $EXIT_STATE
         ;;
 
